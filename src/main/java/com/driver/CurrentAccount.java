bb0e78c87b10d71cd73d1709af20e2829a2945a9
nikhil vanamala <nickhil215@gmail.com>
2022-12-30 16:53:56 +0530

    1st

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -3,9 +3,10 @@ package com.driver;
 public class CurrentAccount extends BankAccount{
     String tradeLicenseId; //consists of Uppercase English characters only
 
-    public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
-        // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
 
+    public CurrentAccount(String name, double balance, double minBalance, String tradeLicenseId) {
+        super(name, balance, 5000);
+        this.tradeLicenseId = tradeLicenseId;
     }
 
     public void validateLicenseId() throws Exception {
@@ -13,7 +14,43 @@ public class CurrentAccount extends BankAccount{
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
+        char [] license=tradeLicenseId.toCharArray();
+        int i=0;
+        int j=license.length-1;
+        while (i<license.length){
+            while (j>=0){
 
+            }
+        }
+
+    }
+    public String reorganizeString(String S) {
+        char ch[] = new char[26];
+        int max = 0;
+        for(char c: S.toCharArray()) {
+            ch[c - 'a'] ++;
+            if(ch[c-'a'] > ch[max]) max = c - 'a';
+        }
+        int len = S.length();
+        if(len < 2 * ch[max] - 1) return "";
+        int index = 0;
+        char []res = new char[len];
+        for(int i = 0 ; i < ch[max]; i++) {
+            res[index] = (char)(max + 'a');
+            index += 2;
+        }
+        ch[max] = 0;
+        for(int i = 0 ; i < 26; i++) {
+            int count = ch[i];
+            while(count > 0 ) {
+                if(index >= len ) index = 1;
+                res[index] = (char)(i + 'a');
+                index += 2;
+                count --;
+            }
+        }
+
+        return new String(res);
     }
 
 }
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,6 +2,10 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
+        SavingsAccount nik=new SavingsAccount("Nikhil",5000,500,4);
+
+        System.out.println(nik.getSimpleInterest(1));
+        System.out.println(nik.getCompoundInterest(2,1));
 
     }
 }
